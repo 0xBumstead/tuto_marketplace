678fb79a4db2c659dd31c26c011108f5d2aaa3c1
fmcch <fmhcry@gmail.com>
2021-08-27 19:44:42 +0200

    Updates

---
+++ precache-manifest.2a2004e38358440c73bf3d045c59426c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/tuto_marketplace/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "f267f8a0277de02889a8",
+    "url": "/tuto_marketplace/static/js/runtime~main.f267f8a0.js"
+  },
+  {
+    "revision": "c9b38980885c2e7c751f",
+    "url": "/tuto_marketplace/static/js/main.c9b38980.chunk.js"
+  },
+  {
+    "revision": "aefffaecdf6c8596fe12",
+    "url": "/tuto_marketplace/static/js/1.aefffaec.chunk.js"
+  },
+  {
+    "revision": "c9b38980885c2e7c751f",
+    "url": "/tuto_marketplace/static/css/main.6c5e9c79.chunk.css"
+  },
+  {
+    "revision": "aefffaecdf6c8596fe12",
+    "url": "/tuto_marketplace/static/css/1.f6d0621f.chunk.css"
+  },
+  {
+    "revision": "9d4447409b7a8a2878d15d01e8d5876c",
+    "url": "/tuto_marketplace/index.html"
+  }
+];
